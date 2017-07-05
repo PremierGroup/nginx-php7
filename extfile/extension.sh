189569e1f54ae471b53196ed2144c5d102356c1c
Cinder <cinder@cinderblocks.biz>
2017-07-05 13:19:58 -0500

    Readd redis extension

--- extfile/extension.sh
+++ extfile/extension.sh
@@ -14,3 +14,10 @@ cd /home/extension/xdebug-XDEBUG_2_4_0RC3 && \
 ./configure --enable-xdebug --with-php-config=/usr/local/php/bin/php-config && \
 make && make install
 
+#Add extension redis
+curl -Lk https://github.com/phpredis/phpredis/archive/3.1.2.tar.gz | gunzip | tar x -C /home/extension && \
+cd /home/extension/phpredis-3.1.2 && \
+/usr/local/php/bin/phpize && \
+./configure --with-php-config=/usr/local/php/bin/php-config && \
+make && make install
+
